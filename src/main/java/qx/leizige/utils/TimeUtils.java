1a9c1d144498307dcdbf51222f1da5441e0dead2
“leizige” <qx_leizige@163.com>
2022-05-28 17:22:16 +0800

    update TimeUtils

--- src/main/java/qx/leizige/utils/TimeUtils.java
+++ src/main/java/qx/leizige/utils/TimeUtils.java
@@ -1,6 +1,7 @@
 package qx.leizige.utils;
 
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -36,18 +37,40 @@ public class TimeUtils implements StringPool {
 
     public static LocalDateTime toLocalDateTime(String text) {
         Objects.requireNonNull(text, "text");
-        DateTimeFormatter formatter = null;
-        if (text.contains("T") && text.contains("Z")) {
-            formatter = DateTimeFormatter.ofPattern(T_DATE_TIME_FORMATTER_PATTERN);
-        } else {
-            formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMATTER_PATTERN);
+        try {
+            Long time = Long.parseLong(text);
+            return toLocalDateTime(time);
         }
-        return LocalDateTime.parse(text, formatter);
+        catch (Exception e) {
+            DateTimeFormatter formatter;
+            if (text.contains("T") && text.contains("Z")) {
+                formatter = DateTimeFormatter.ofPattern(T_DATE_TIME_FORMATTER_PATTERN);
+            }
+            else {
+                formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMATTER_PATTERN);
+            }
+            return LocalDateTime.parse(text, formatter);
+        }
     }
 
     public static LocalDate toLocalDate(String text) {
         Objects.requireNonNull(text, "text");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER_PATTERN);
         return LocalDate.parse(text, formatter);
+    }
+
+    /**
+     *时间戳转换为日期类型格式
+     * @param time 1612518379000
+     * @return 2021-02-05 05:46:19
+     */
+    public static LocalDateTime toLocalDateTime(Long time) {
+        Instant instant = Instant.ofEpochMilli(time);
+        ZoneId zone = ZoneId.systemDefault();
+        return LocalDateTime.ofInstant(instant, zone);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toLocalDateTime(1612518379000L));
     }
 }
